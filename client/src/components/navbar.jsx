3368043b5156e8391657e0c37f07859f31999a53
ikhushisingla <singlakhushi822@gmail.com>
2024-03-12 23:46:24 +0530

    logout functionality added

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,20 +1,35 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import useUserState from "@/hooks/use-user";
+import { useRouter } from "next/navigation";
 
-export default function Navbar() {
-    const user=useUserState(state=>state.user);
+ const Navbar=()=> {
+    const {user,setUser}=useUserState(state=>state);
+    const router=useRouter()
+    const handleSubmit=async()=>{
+        if(user.isVerified){
+          setUser(null)
+          router.replace('/')
+        }
+    }
+    
+
   return (
-    <div
-      className=
-      "bg-slate-950  top-10 inset-x-0 w-full text-white px-8 py-4 mx-auto z-50"
-    >
+    <div className="bg-slate-950  top-10 inset-x-0 w-full text-white px-8 py-4 mx-auto z-50">
         <div className="ml-auto w-fit">
-{user?<h1>{user.name}</h1>:<Link href='/login' className="hover:text-slate-400 hover:underline transition-all">Login</Link>
-}
+            {user?
+            <div className="flex gap-4">
+              <h1>{user.name}</h1>
+              <button type="submit" onClick={handleSubmit} className="hover:text-slate-400 hover:underline transition-all">LogOut</button>
+            </div>
+              
+              :
+              <Link href='/login' className="hover:text-slate-400 hover:underline transition-all">Login</Link>
+            }
         </div>
     </div>
   );
 }
+export default Navbar

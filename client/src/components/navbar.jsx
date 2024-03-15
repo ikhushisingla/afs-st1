8861ede056ff08b36ce7eb3dd3f72433972f9b3d
ikhushisingla <singlakhushi822@gmail.com>
2024-03-15 14:43:43 +0530

    loading state added

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -14,7 +14,6 @@ import { useRouter } from "next/navigation";
           router.replace('/')
         }
     }
-    
 
   return (
     <div className="bg-slate-950  top-10 inset-x-0 w-full text-white px-8 py-4 mx-auto z-50">
@@ -24,9 +23,8 @@ import { useRouter } from "next/navigation";
               <h1>{user.name}</h1>
               <button type="submit" onClick={handleSubmit} className="hover:text-slate-400 hover:underline transition-all">LogOut</button>
             </div>
-              
               :
-              <Link href='/login' className="hover:text-slate-400 hover:underline transition-all">Login</Link>
+            <Link href='/login' className="hover:text-slate-400 hover:underline transition-all">Login</Link>
             }
         </div>
     </div>
